125a9ffa62901d52a616dbc5dbf61dc27e5e2afa
Vinh VO <voldedore@gmail.com>
2019-04-17 17:25:48 +0700

    Add initDB with init flag

--- main.go
+++ main.go
@@ -1,15 +1,62 @@
 package main
 
 import (
-	"time"
+	"database/sql"
 	"log"
-//	"fmt"
-//	"strconv"
-//	"net/url"
 	"os"
+	"time"
 
+	_ "github.com/mattn/go-sqlite3"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
+
+const DB_NAME string = "news.db"
+const NEWS_SRC_TINHTE_LABEL string = "tinhte"
+const NEWS_SRC_BING_LABEL string = "bing"
+const INITIALIZED_FLAG_FILE_NAME string = "init.done"
+
+func initDB() {
+	// Create DB
+	os.Remove(DB_NAME)
+	db, err := sql.Open("sqlite3", DB_NAME)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+    create table news_content (id integer not null primary key, source text, url text);
+    `
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+}
+
+func prepDB() {
+
+	// Try to open the flag, if it's not exist, then init the file
+	if file, err := os.Open(INITIALIZED_FLAG_FILE_NAME); os.IsNotExist(err) {
+		file.Close()
+
+		// Write the flag
+		file, err := os.Create(INITIALIZED_FLAG_FILE_NAME)
+		if err != nil {
+			log.Fatal("Flag create failed")
+			return
+		}
+		file.Close()
+		initDB()
+
+	} else {
+		log.Println("This seems inited")
+		if file, err := os.Open(DB_NAME); os.IsNotExist(err) {
+			file.Close()
+			initDB()
+		} // This should validate DB, a bit overkill for this little project
+	}
+}
 
 // News Bot
 func newsBot() {
@@ -24,16 +71,15 @@ func newsBot() {
 	}
 
 	b.Handle(tb.OnText, func(m *tb.Message) {
-		b.Send(m.Sender, "This bot does not currently supported the interactive mode")
-
+		b.Send(m.Sender, "This bot does not currently support the interactive mode")
 	})
 
 	// Getting the Channel
 	channel, channelGetErr := b.ChatByID(os.Getenv("CHANNEL_ID"))
 
 	if channelGetErr != nil {
-	    log.Fatal(channelGetErr)
-	    return
+		log.Fatal(channelGetErr)
+		return
 	}
 
 	b.Send(channel, "This test message")
@@ -42,7 +88,7 @@ func newsBot() {
 }
 
 func main() {
-	go newsBot()	
+	go prepDB()
+	go newsBot()
 	select {}
 }
-
